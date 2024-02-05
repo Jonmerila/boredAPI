4436b5d0f57d1ed0273902c3fc6041fb11074de5
Jon Merilä <jon.merila97@hotmail.com>
2024-02-05 11:44:25 +0100

    Wrote out actitivy info

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ const activityTitle = document.querySelector("#activityTitle");
 
 const getData = async (url) => {
   const response = await fetch(url);
-  const json = response.json();
+  const json = await response.json();
   activityTitle.innerText = json.activity;
+  activityContainer.innerHTML = "";
+  const activityList = {
+    type: json.type,
+    participants: json.participants,
+    price: json.price,
+  };
+  Object.keys(activityList).forEach((key) => {
+    let p = document.createElement("p");
+    p.textContent = `${key} : ${activityList[key]}`;
+    activityContainer.append(p);
+  });
+
+  console.log(json);
 };
 // const buildUrl = () => {
 //   return "https://www.boredapi.com/api/activity";
@@ -13,5 +26,7 @@ const getData = async (url) => {
 
 const url = "https://www.boredapi.com/api/activity";
 
-console.log(url);
-getActivity.addEventListener("click", getData("https://www.boredapi.com/api/activity"));
+// console.log(getData("https://www.boredapi.com/api/activity"));
+getActivity.addEventListener("click", () =>
+  getData("https://www.boredapi.com/api/activity")
+);
