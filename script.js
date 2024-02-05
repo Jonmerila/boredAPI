5c1e3facab64dda8c62ab863567536028a69cea8
Jon Merilä <jon.merila97@hotmail.com>
2024-02-05 15:51:42 +0100

    Finished filter function

--- script.js
+++ script.js
@@ -3,43 +3,60 @@ const activityContainer = document.querySelector("#activityContainer");
 const activityTitle = document.querySelector("#activityTitle");
 
 const getData = async (url) => {
-  const response = await fetch(url);
-  const json = await response.json();
-  activityTitle.innerText = json.activity;
-  activityContainer.innerHTML = "";
-  const activityList = {
-    type: json.type,
-    participants: json.participants,
-    price: json.price,
-  };
-  Object.keys(activityList).forEach((key) => {
-    let p = document.createElement("p");
-    p.textContent = `${key} : ${activityList[key]}`;
-    activityContainer.append(p);
-  });
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    if (json.activity == undefined) {
+      throw new Error("Not valid!");
+    }
+    activityTitle.innerText = json.activity;
+    activityContainer.innerHTML = "";
+    const activityList = {
+      type: json.type,
+      participants: json.participants,
+      price: json.price,
+    };
 
-  //   console.log(json);
+    Object.keys(activityList).forEach((key) => {
+      let p = document.createElement("p");
+      p.textContent = `${key} : ${activityList[key]}`;
+      activityContainer.append(p);
+    });
+
+    //   console.log(json);
+  } catch (err) {
+    let errorTitle = document.createElement("h1");
+    errorTitle.textContent = "Error, not found";
+    activityContainer.append(errorTitle);
+  }
 };
 
-const buildUrl = async (participants, checkbox, dropdown) => {
+const buildUrl = (participants, checkbox, dropdown) => {
   let params = new URLSearchParams();
-  params.append("participants", participants.value);
-  if (checkbox.value == true) {
-    params.append("maxprice", 0.0);
+  if (participants.value !== "any") {
+    params.append("participants", participants.value);
   }
-  params.append("type", dropdown.value);
-  let url = await getData("http://www.boredapi.com/api/activity?" + params);
-    console.log(url);
-    return url;
+  if (checkbox.checked) {
+    params.append("price", 0.0);
+  }
+  if (dropdown.value !== "any") {
+    params.append("type", dropdown.value);
+  }
+  let url = "http://www.boredapi.com/api/activity?" + params;
+  console.log(url);
+  return url;
 };
 
 // console.log(getData("https://www.boredapi.com/api/activity"));
-getActivity.addEventListener("click", () => {
+getActivity.addEventListener("click", async () => {
   const participants = document.querySelector('input[type="radio"]:checked');
   const checkBox = document.querySelector("#free");
   const dropDown = document.querySelector("#dropdown");
-  getData("https://www.boredapi.com/api/activity");
- getData(buildUrl(participants, checkBox, dropDown));
+  //   getData("https://www.boredapi.com/api/activity");
+  console.log(buildUrl(participants, checkBox, dropDown));
+  let url = buildUrl(participants, checkBox, dropDown);
+  console.log("url", url);
+  await getData(url);
   //   console.log(participants.value);
   //   console.log(checkBox.checked);
   //   console.log(dropdown.value);
